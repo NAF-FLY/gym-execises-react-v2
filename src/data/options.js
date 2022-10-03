266a8527ec3a3a16fe1ba50f5ac2e382e4b1b214
Vadim <vadim192000@gmail.com>
2022-10-03 20:12:10 +0300

    обновил api

--- src/data/options.js
+++ src/data/options.js
@@ -2,7 +2,7 @@ export const exerciseOptions = {
   method: "GET",
   headers: {
     "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
-    "X-RapidAPI-Key": process.env.REACT_APP_EXERCISE_RAPID_API_KEY,
+    "X-RapidAPI-Key": "53404170fcmsh72fbee6e88301adp107e2cjsnde375a2e1682",
   },
 };
 
